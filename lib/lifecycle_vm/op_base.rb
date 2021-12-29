3e7bc99ae8f4d83b17da87cd865e350c47975311
Alex Scarborough <alex@teak.io>
2021-12-28 16:04:18 -0800

    OpBase docs.

--- lib/lifecycle_vm/op_base.rb
+++ lib/lifecycle_vm/op_base.rb
@@ -17,7 +17,17 @@
 require 'lifecycle_vm/memory'
 
 module LifecycleVM
+  # Base class for all ops in a vm.
+  # An op may read and write to vm memory by declaring reads and writes using the
+  # DSL. The lifecycle of an op is initialize -> call -> validate, and all three
+  # methods will always be called. All reads and logger will be provided before
+  # initialize is called.
+  #
+  # An op may indicate that it encountered an error or otherwise failed by calling
+  # #error. If an op has any errors, no writes will be persisted to the vm memory.
   class OpBase
+    # Raised if an op attempts to read or write to a memory slot that does not
+    # exist, or if an op attempts to write to any slot in LifecycleVM::Memory::PROTECTED.
     class InvalidAttr < ::LifecycleVM::Error
       attr_reader :op_class, :attribute
 
@@ -28,6 +38,7 @@ module LifecycleVM
       end
     end
 
+    # Read one or more values out of vm memory and provide them as readable attributes
     def self.reads(*attrs)
       @reads ||= {}
       @writes ||= {}
@@ -43,6 +54,8 @@ module LifecycleVM
       @reads
     end
 
+    # Allow writing one or more values to vm memory and provide them as writeable attributes
+    # @note You may not write to any slot in LifecycleVM::Memory::PROTECTED
     def self.writes(*attrs)
       @reads ||= {}
       @writes ||= {}
@@ -63,6 +76,9 @@ module LifecycleVM
       @writes
     end
 
+    # Execute the current op with the gien vm memory.
+    # This will read out all declared reads from memory and if executed without
+    # any errors will write all declared writes to memory.
     def self.call(memory)
       obj = allocate
 
@@ -100,12 +116,17 @@ module LifecycleVM
 
     attr_reader :errors, :logger
 
+    # Declare that an error has occurred.
+    # @param field [Symbol] the memory field which is in error. The convention is to use :base
+    #   for errors not associated with any particular memory field.
+    # @param message [Object] a description of the error. Convention is to use a string.
     def error(field, message)
       @errors ||= {}
       @errors[field] ||= []
       @errors[field] << message
     end
 
+    # Did any errors occur in the execution of this op?
     def errors?
       !(@errors.nil? || @errors.empty?)
     end
